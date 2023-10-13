6e27512ca234fdb902789fd4493520e9f32d1679
hwlee11 <hwl@ai.korea.ac.kr>
2023-10-13 19:55:39 +0000

    fix trainer

--- modules/trainer.py
+++ modules/trainer.py
@@ -5,6 +5,25 @@ from dataclasses import dataclass
 import time
 from nova import DATASET_PATH
 
+def greedy_scoring(model, h, h_mask, device, sos=1, eos=2):
+
+    batchSize, _, T = h.size()
+    tokens = torch.tensor([sos] * batchSize).to(device)
+    token_lengths = torch.tensor([1]*batchSize).to(device)
+    sum_logprobs = torch.zeros(2,2000).to(device)
+    for i in range(T):
+        out, out_mask = model.decode_one_step(tokens, token_lengths, h, h_mask)
+        next_token = out.argmax(dim=1)
+        logprobs = F.log_softmax(out[:,:,-1:],dim=1).squeeze(-1)
+        sum_logprobs += logprobs.squeeze(0)
+
+        next_token[tokens[:, -1] == eos] = eos
+        tokens = torch.cat([tokens,next_token],dim=-1)
+        endOfDecode = (tokens[:,-1] == eos).all()
+        if endOfDecode:
+            break
+
+    return tokens, sum_logprobs
 
 def trainer(mode, config, dataloader, optimizer, model, criterion, metric, train_begin_time, device):
 
@@ -27,17 +46,17 @@ def trainer(mode, config, dataloader, optimizer, model, criterion, metric, train
 
         if mode == 'train':
             outputs, output_lengths = model(inputs, input_lengths)
+            loss = criterion(
+                outputs.transpose(0, 1),
+                targets[:, 1:],
+                tuple(output_lengths),
+                tuple(target_lengths)
+            )
+            y_hats = outputs.max(-1)[1]
+
         elif mode == 'valid':
-            outputs, output_lengths = model.infer(inputs,input_legnths)
-
-        loss = criterion(
-            outputs.transpose(0, 1),
-            targets[:, 1:],
-            tuple(output_lengths),
-            tuple(target_lengths)
-        )
-
-        #y_hats = outputs.max(-1)[1]
+            outputs, output_lengths = model.encoder_forward(inputs, input_legnths)
+            y_hats, loss = greedy_scoring(model, outputs, outputs_lengths, device)
 
         if mode == 'train':
             optimizer.zero_grad()
@@ -56,11 +75,11 @@ def trainer(mode, config, dataloader, optimizer, model, criterion, metric, train
             elapsed = current_time - begin_time
             epoch_elapsed = (current_time - epoch_begin_time) / 60.0
             train_elapsed = (current_time - train_begin_time) / 3600.0
-            #cer = metric(targets[:, 1:], y_hats)
+            cer = metric(targets[:, 1:], y_hats)
             print(log_format.format(
                 cnt, len(dataloader), loss,
                 elapsed, epoch_elapsed, train_elapsed,
                 optimizer.get_lr(),
             ))
         cnt += 1
-    return model, epoch_loss_total/len(dataloader)#, metric(targets[:, 1:], y_hats)
+    return model, epoch_loss_total/len(dataloader), metric(targets[:, 1:], y_hats)
